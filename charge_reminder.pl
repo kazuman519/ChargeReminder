d57646229ef7a037370743188331c8cc9a110e77
miura <miura@mfac.jp>
2015-06-29 13:13:23 +0900

    update bot system

--- charge_reminder.pl
+++ charge_reminder.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use utf8;
+use Encode;
 use MF::Notify::Slack;
 use DateTime;
 use DBI;
@@ -11,20 +12,24 @@ use Data::Dumper;
 my $database_name = 'chargeMember';
 my $table_name = 'charge';
 
-my $charger = '';
 my $work_name = $ARGV[0];
+my $charger   = '';
+my $channel   = '';
+my $text      = '';
 
 
 # ポストするデータを取得する関数
-sub getPostData {
+sub createPostData {
 
-  my($work_name, $charger) = @_;
+  my($charger ,$channel, $text) = @_;
   print "charger -> $charger\n";
 
-  my $br = "\r\n";
+  # エンコード&文字置き換え
+  $text = Encode::decode('utf-8', $text);
+  $text =~s/_NAME_/$charger/g;
+  $text =~s/_BR_/\n/g;
+
   my $conf = {
-    priv_members => {
-    },
     post_data => {
       default => {
         channel    => '@miura',
@@ -32,30 +37,15 @@ sub getPostData {
         username   => 'MCお知らせ',
         text       => '',
       },
-      test_msg => {
-        channel => '@miura',
-        text    => 'すみませんテストです$charger  これでおわり<!group>',
-      },
-      mf_mtg_morning => {
-        channel => '#mc-doraemon',
-        text    => '本日の朝会の発表者は'.$charger.'さんです！'.$br.'よろしくお願いします！<!group>',
-      },
-      mc_mtg_frontend => {
-        channel => '#mc-doraemon',
-        text    => '本日のMCフロントエンドMTGの司会&Wiki準備する人は'.$charger.'さんです！'.$br.'http://wiki.mf.local/tag/MC-FRONTEND-MTG'.$br.'よろしくお願いします！<!group>',
-      },
-      mc_mtg_develop => {
-        channel => '#mc-doraemon',
-        text    => '本日のMC開発MTGの司会&Wiki準備する人は'.$charger.'さんです！'.$br.'http://wiki.mf.local/tag/MC-ENGINEER-MTG'.$br.'よろしくお願いします！<!group>',
+      custom => {
+        channel    => $channel,
+        text       => "$text",
       },
     },
   };
 
-  # 引数に対応するデータがなければ何もせずに死ぬ
-  die if !$work_name or !$conf->{post_data}->{$work_name};
-
   # デフォルトデータを引数で指定されたデータで上書き
-  my %return_data = (%{$conf->{post_data}->{default}}, %{$conf->{post_data}->{$work_name}});
+  my %return_data = (%{$conf->{post_data}->{default}}, %{$conf->{post_data}->{custom}});
 
   return %return_data;
 }
@@ -68,6 +58,7 @@ $dbh = DBI->connect(
   "root",
   ""
 );
+$dbh->do("set names utf8");
 
 ## プリペアードステートメント
 my $sth;
@@ -76,25 +67,34 @@ my $ref;
 ## 発表者の取得
 my $next_charger_number = 1;
 my @member = '';
+
 $sth = $dbh->prepare("select * from $table_name where work_name = ?");
 $sth->execute($work_name);
 while($ref = $sth->fetchrow_hashref()){
   # メンバーと次発表する人の番号を取得
-  my $member_string = $ref->{'member'};
+  my $member_string    = $ref->{'member'};
   $next_charger_number = $ref->{'next_charger_number'};
+  $channel             = $ref->{'channel'};
+  $text                = $ref->{'text'};
 
   if(index($member_string,",") != -1){
     @member = split(/,/,$member_string);
     if ($next_charger_number <= @member) {
       $charger = $member[$next_charger_number-1];
     }
+  } else {
+    $charger = $member_string;
   }
 }
 $sth->finish();
 
+# 引数に対応するworkがないか、引数がなければ何もせずに死ぬ
+die if !$work_name or $charger eq '';
+
 ## 発言者番号のインクリメント
 $next_charger_number++;
 if ($next_charger_number > @member ) {
+  # メンバー数以上の番号になっていたら最初に戻す
   $next_charger_number = 1;
 }
 
@@ -107,6 +107,7 @@ $sth->finish();
 $dbh->disconnect();
 
 # slackに通知
-MF::Notify::Slack->new->post(&getPostData($work_name, $charger));
+my %postData = &createPostData($charger, $channel, $text);
+MF::Notify::Slack->new->post(%postData);
 
 exit;
